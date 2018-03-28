855db4df2d3b495ece3d1d2dd03fd57e4e6c5d5e
Stephanie Boehning <stephanie.boehning@haw-hamburg.de>
2018-03-28 16:18:27 +0200

    ohne Warnings

--- Vorlesung P2/vorlesung03/complexGui/ComplexLayout.java
+++ Vorlesung P2/vorlesung03/complexGui/ComplexLayout.java
@@ -11,6 +11,11 @@ import javax.swing.JTextArea;
 
 public class ComplexLayout extends JFrame{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public ComplexLayout() {
 		
 		JPanel westPanel = new JPanel();
