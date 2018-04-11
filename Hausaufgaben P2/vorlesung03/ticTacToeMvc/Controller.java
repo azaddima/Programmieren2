74b293fbfe55f5a4648a36981a555b943aa4247b
Stephanie Boehning <stephanie.boehning@haw-hamburg.de>
2018-04-11 12:05:02 +0200

    erweitert um Dialoge und Menüs

--- Hausaufgaben P2/vorlesung03/ticTacToeMvc/Controller.java
+++ Hausaufgaben P2/vorlesung03/ticTacToeMvc/Controller.java
@@ -4,16 +4,35 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
 public class Controller {
 	
 	public Controller(GUI gui, GameLogic gameLogic) {
+		
+		gui.getAbout().addActionListener(l -> {
+//			new AboutDialog(gui, "About", true);
+			JOptionPane.showMessageDialog(gui, "bla");
+		});
+		
 		for (JButton btn : gui.getButtons()) {
+			gui.btn0.addActionListener(l -> {
+				if (gameLogic.setSymbol(0)) {
+					if (gameLogic.isGameOver()) {
+						gui.update(0);								
+						gui.setGameOver(gameLogic.getGewinner());
+					} else {
+						gui.update(0);								
+					}
+				}
+
+			});
+			
 			btn.addActionListener(new ActionListener() {
 				
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					String str = e.getActionCommand();
+					String str = e.getActionCommand();	//hole den Index von dem Button
 					try {
 						int index = Integer.valueOf(str);						
 						if (gameLogic.setSymbol(index)) {
--- Hausaufgaben P2/vorlesung03/ticTacToeMvc/GUI.java
+++ Hausaufgaben P2/vorlesung03/ticTacToeMvc/GUI.java
@@ -9,6 +9,9 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 public class GUI extends JFrame {
@@ -28,14 +31,33 @@ public class GUI extends JFrame {
 	private ImageIcon player2Img = new ImageIcon("vorlesung03//ticTacToeMvc//x.jpg");
 
 	private ArrayList<JButton> buttons = new ArrayList<>();
+	public JButton btn0 = new JButton("");
+	public JButton btn1 = new JButton("");
+	public JButton btn2 = new JButton("");
 	private GameLogic gameLogic;
+	
+	private JMenuItem close = new JMenuItem("Close");
+	private JMenuItem restart = new JMenuItem("Restart");
+	private JMenuItem about = new JMenuItem("About");
 	
 	public GUI(GameLogic gameLogic) {
 		this.gameLogic = gameLogic;
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		JMenu file = new JMenu("File");
+		menuBar.add(file);
+		file.add(restart);
+		file.add(close);
+		file.add(about);
 
 		//das Grid mit den Button wird aufgebaut
 		JPanel grid = new JPanel();
 		grid.setLayout(new GridLayout(gameLogic.getSize(), gameLogic.getSize()));
+		grid.add(btn0);
+		grid.add(btn1);
+		grid.add(btn2);
+		
 		for (int i = 0; i < gameLogic.getSize()*gameLogic.getSize(); i++) {
 			JButton button = new JButton("");
 			//die Button bekommen den Index als action command
@@ -54,6 +76,14 @@ public class GUI extends JFrame {
 		setVisible(true);
 		setSize(550, 550);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public JMenuItem getAbout() {
+		return about;
+	}
+	
+	public JMenuItem getClose() {
+		return close;
 	}
 	
 	public ArrayList<JButton> getButtons() {
